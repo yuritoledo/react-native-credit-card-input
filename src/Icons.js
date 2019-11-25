9774b09d6b975291bcb3f39bbb60025778ac1345
Jonathan Tafel <tafelito@gmail.com>
2019-11-25 15:33:43 -0500

    Fix mastercard icon

--- src/Icons.js
+++ src/Icons.js
@@ -3,7 +3,7 @@ const Icons = {
   cvc_amex: require('./icons/stp_card_cvc_amex.png'),
   'american-express': require('./icons/stp_card_amex.png'),
   'diners-club': require('./icons/stp_card_diners.png'),
-  'master-card': require('./icons/stp_card_mastercard.png'),
+  mastercard: require('./icons/stp_card_mastercard.png'),
   discover: require('./icons/stp_card_discover.png'),
   jcb: require('./icons/stp_card_jcb.png'),
   placeholder: require('./icons/stp_card_unknown.png'),
